2c23880a67591cba0c3d4d40b98a44fd6236de68
Devendar Sai Thallam <programmingwithdev@gmail.com>
2024-01-26 22:22:29 -0500

    GameOver when Bird goes out of screen

--- Assets/BirdScript.cs
+++ Assets/BirdScript.cs
@@ -19,11 +19,20 @@ public class BirdScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space) && isBirdAlive){
+        if((Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0) || (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began )) && isBirdAlive){
             myRigidBody.velocity = Vector2.up * flapStrength;
         }
+        CheckIfBirdIsOutOfScreen();
     }
+    private void CheckIfBirdIsOutOfScreen(){
+        Vector2 viewportPosition = Camera.main.WorldToViewportPoint(transform.position);
 
+        if (viewportPosition.y > 1 || viewportPosition.y < 0)
+        {
+            logic.gameOver();
+            isBirdAlive = false;
+        }
+    }
     private void OnCollisionEnter2D(Collision2D collision){
         logic.gameOver();
         isBirdAlive = false;
